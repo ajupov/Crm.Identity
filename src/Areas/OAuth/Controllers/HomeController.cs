82d29cf250557d490c3ed87f1c147c0bb84d4d2f
Usman Ayupov <u.ayupov@dodopizza.com>
2019-12-14 00:14:38 +0300

    Add logging

--- src/Areas/OAuth/Controllers/HomeController.cs
+++ src/Areas/OAuth/Controllers/HomeController.cs
@@ -1,22 +1,33 @@
 ﻿using System.Reflection;
 using Ajupov.Infrastructure.All.Mvc;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Crm.Identity.Areas.OAuth.Controllers
 {
     [Route("")]
     public class HomeController : DefaultMvcController
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("")]
         public ActionResult Index()
         {
             var assembly = Assembly.GetEntryAssembly();
             var attribute = assembly?.GetCustomAttribute<AssemblyFileVersionAttribute>();
 
-            var name = assembly?.GetName()?.Name;
+            var name = assembly?.GetName().Name;
             var version = attribute?.Version;
+            var message = $"{name} {version}";
 
-            return Content($"{name} {version}");
+            _logger.LogInformation("Index request. message: {0}", message);
+
+            return Content(message);
         }
     }
 }
