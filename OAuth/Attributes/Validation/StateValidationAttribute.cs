5f1ac5179bdae47aafa325b3b04dc4cb834db3c2
Usman Ayupov <u.ayupov@dodopizza.com>
2020-01-06 21:40:45 +0300

    Remove state lenth validation

--- OAuth/Attributes/Validation/StateValidationAttribute.cs
+++ OAuth/Attributes/Validation/StateValidationAttribute.cs
@@ -5,12 +5,10 @@ namespace Crm.Identity.OAuth.Attributes.Validation
 {
     public class StateValidationAttribute : ValidationAttribute
     {
-        private const int StateLength = 8;
-
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var stateString = value?.ToString();
-            if (stateString.IsEmpty() || stateString?.Length != StateLength)
+            if (stateString.IsEmpty())
             {
                 return new ValidationResult("Invalid state value");
             }
